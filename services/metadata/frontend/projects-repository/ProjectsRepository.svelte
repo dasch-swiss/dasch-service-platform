99746fc9bb6e753a2147b6e6ee6249acc8d43d1c
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-03-31 12:17:57 +0200

    cleaned FE part structure

---
+++ services/metadata/frontend/projects-repository/ProjectsRepository.svelte
@@ -0,0 +1,103 @@
+<script lang='ts'>
+  import Tile from './Tile.svelte';
+  import Category from './Category.svelte';
+  import { onMount } from 'svelte';
+  import type { PaginationData, Project } from '../interfaces';
+  import Pagination from './Pagination.svelte';
+  import { getProjects, pagedResults, pages } from '../stores';
+
+  let projects: Project[];
+  let message = 'Loading...';
+  let pagination: PaginationData;
+
+  setTimeout(() => {
+    const noData = 'No data retrived. Please check the connection and retry.';
+    const noProject = 'No projects found.'
+      message = projects && projects.length ? noData : noProject;
+    }, 3000);
+  
+  onMount(async () => {
+    await getProjects(1);
+
+    pagedResults.subscribe(r => projects = r);
+
+    pages.subscribe(p => pagination = p);
+  });
+</script>
+
+<nav>
+  <div class="category-container hidden m-inline-block">
+    <Category bind:searched={projects} />
+  </div>
+</nav>
+<main>
+  <div class=tile-container>
+    {#if projects && projects.length}
+      {#each projects as project}
+        <Tile project={project}/>
+      {/each}
+    {:else}
+      <p>{message}</p>
+    {/if}
+  </div>
+  {#if projects && projects.length}
+    <Pagination pagination={pagination} />
+  {/if}
+</main>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+nav, main {
+  width: 100%;
+  min-height: auto;
+  padding: 10px;
+}
+nav {
+  flex: 0 0 20%;
+  /* background-color: hotpink; */
+  display: flex;
+  justify-content: flex-end;
+  padding: 0;
+}
+.category-container {
+  /* background-color: lemonchiffon; */
+  padding-top: 45px;
+  max-width: 210px;
+}
+main {
+  width: 100%;
+  /* background-color: aqua; */
+  align-items: center;
+  justify-content: center;
+}
+.tile-container {
+  padding: 40px 5px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  /* background-color: skyblue; */
+  max-width: 1200px;
+}
+@media screen and (min-width: 992px) {
+  nav, main {
+    min-height: 950px;
+  }
+  nav {
+    padding: 10px;
+  }
+  .tile-container {
+    padding: 40px 0;
+    min-width: 742px;
+  }
+}
+@media screen and (min-width: 1200px) {
+  .tile-container {
+    min-width: 940px;
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 1023px) { }
+@media screen and (min-width: 1024px) and (max-width: 1365px) { }
+@media screen and (min-width: 1366px) {}
+</style>
---
+++ services/metadata/frontend/projects-repository/Tile.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import type { Project } from '../interfaces';
+  import { currentProject } from '../stores';
+  
+  export let project: Project;
+
+  let setCurrentProject = () => {
+    currentProject.set(project);
+  }
+</script>
+
+<section>
+  <div class=header>
+    <h5>{ project.name }</h5>
+  </div>
+  <div class=content>{ project.description }</div>
+  <div class=footer>
+    <a on:click={setCurrentProject} href="#/project/{project.id}">Read more</a>
+  </div>
+</section>
+
+<style>
+  section {
+    border: 1px solid #000;
+    border-radius: 5px;
+    background-color: #fff;
+    padding: 5px 5vw;
+    margin: 5px;
+    width: 75vw;
+    float: left;
+  }
+  .header{
+    height: 30px;
+    padding-top: 10px;
+  }
+  .header h5 {
+    text-overflow: ellipsis;
+    overflow: hidden; 
+    white-space: nowrap;
+    margin: 0;
+  }
+  .content {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    font-size: 0.8em;
+    height: 45x;
+    margin: 10px 0 25px;
+  }
+  .footer {
+    font-size: 0.8em;
+    margin: 10px 0;
+    color: var(--dasch-violet);
+  }
+  .footer a {
+    display: flex;
+    justify-content: center;
+    height: 20px;
+  }
+  @media screen and (min-width: 768px) {
+    section {
+      width: 240px;
+      height: 208px;
+      padding: 10px 30px;
+      margin: 10px;
+    }
+    .content {
+      -webkit-line-clamp: 7;
+      height: 107px;
+      margin: 5px 0 25px;
+    }
+  }
+  @media screen and (min-width: 1200px) {
+    section {
+      margin: 5px;
+    }
+  }
+</style>
