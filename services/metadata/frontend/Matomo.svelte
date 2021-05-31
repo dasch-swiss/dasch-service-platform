6aacbcbc14e9828487b51300a5ce33aa47673bc4
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-05-31 10:56:36 +0200

    excluded local env from tracking

--- services/metadata/frontend/Matomo.svelte
+++ services/metadata/frontend/Matomo.svelte
@@ -2,26 +2,29 @@
 <!-- TODO: doesn't work on pagination browse -->
 <!-- full guide: https://developer.matomo.org/guides/tracking-javascript-guide -->
 <script>
-  var _paq = window._paq = window._paq || [];
-  _paq.push(['setCustomUrl', window.location.href]);
-  _paq.push(['setDocumentTitle', document.title]);
-  _paq.push(['trackPageView']);
-  // Enable Download & Outlink tracking but doesn't track outlinks when opened in new tab
-  // to track json, ttl, etc use setDownloadExtensions
-  _paq.push(['enableLinkTracking']);
-  // adding a class to external links should make them trackable even if opened in separate tab
-  // if not this class can be removed across the app
-  _paq.push(['setLinkClasses', "external-link"]);
-  // Make Matomo aware of newly added content
-  // TODO: check if below works when media or forms are available
-  _paq.push(['MediaAnalytics::scanForMedia', document]);
-  _paq.push(['FormAnalytics::scanForForms', document]);
-  _paq.push(['trackContentImpressionsWithinNode', document]);
-  (function() {
-    var u="https://dasch.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/dasch.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
+  const url = window.location;
+  if (url.hostname !== 'localhost') {
+    let _paq = window._paq = window._paq || [];
+    _paq.push(['setCustomUrl', url.href]);
+    _paq.push(['setDocumentTitle', document.title]);
+    _paq.push(['trackPageView']);
+    // Enable Download & Outlink tracking but doesn't track outlinks when opened in new tab
+    // to track json, ttl, etc use setDownloadExtensions
+    _paq.push(['enableLinkTracking']);
+    // adding a class to external links should make them trackable even if opened in separate tab
+    // if not this class can be removed across the app
+    _paq.push(['setLinkClasses', 'external-link']);
+    // Make Matomo aware of newly added content
+    // TODO: check if below works when media or forms are available
+    _paq.push(['MediaAnalytics::scanForMedia', document]);
+    _paq.push(['FormAnalytics::scanForForms', document]);
+    _paq.push(['trackContentImpressionsWithinNode', document]);
+    (function() {
+      let u='https://dasch.matomo.cloud/';
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '2']);
+      let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/dasch.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+  }
 </script>
