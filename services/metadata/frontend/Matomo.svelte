4348bb542f39831aa92997612505f4e85279886e
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-05-31 10:42:54 +0200

    added more tracking options

--- services/metadata/frontend/Matomo.svelte
+++ services/metadata/frontend/Matomo.svelte
@@ -1,11 +1,22 @@
 <!-- TODO: try to place it in store -->
 <!-- TODO: doesn't work on pagination browse -->
-<script type="text/javascript">
+<!-- full guide: https://developer.matomo.org/guides/tracking-javascript-guide -->
+<script>
   var _paq = window._paq = window._paq || [];
   _paq.push(['setCustomUrl', window.location.href]);
   _paq.push(['setDocumentTitle', document.title]);
   _paq.push(['trackPageView']);
+  // Enable Download & Outlink tracking but doesn't track outlinks when opened in new tab
+  // to track json, ttl, etc use setDownloadExtensions
   _paq.push(['enableLinkTracking']);
+  // adding a class to external links should make them trackable even if opened in separate tab
+  // if not this class can be removed across the app
+  _paq.push(['setLinkClasses', "external-link"]);
+  // Make Matomo aware of newly added content
+  // TODO: check if below works when media or forms are available
+  _paq.push(['MediaAnalytics::scanForMedia', document]);
+  _paq.push(['FormAnalytics::scanForForms', document]);
+  _paq.push(['trackContentImpressionsWithinNode', document]);
   (function() {
     var u="https://dasch.matomo.cloud/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
