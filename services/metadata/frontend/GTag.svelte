e925fb81a341719a6388781c2142567f4c1f3c33
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-06-03 09:21:35 +0200

    refactor: moved gtag script to separate file

---
+++ services/metadata/frontend/GTag.svelte
@@ -0,0 +1,13 @@
+<script>
+  // enable GA for PROD env only
+  const hostname = 'meta.dasch.swiss';
+  // const hostname = 'localhost';
+
+  if(window.location.hostname === hostname) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-9MD8RKZJHE');
+  }
+</script>
