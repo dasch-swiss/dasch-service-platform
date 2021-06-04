c3371f08424ffc6097f2c4d995071f77362f858f
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-06-04 13:43:12 +0200

    fix: expiring days set to 90

--- services/metadata/frontend/cookies-service.ts
+++ services/metadata/frontend/cookies-service.ts
@@ -7,7 +7,7 @@ declare global {
 
 import {writable} from "svelte/store";
 
-export const setCookie = (name: string, value: string, days = 365, path = '/') => {
+export const setCookie = (name: string, value: string, days = 90, path = '/') => {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
   document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=${path}`;
 };
