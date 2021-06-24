843b5c8a0157d6f165fd9b259090e409b2acb53e
Balduin Landolt <balduin.landolt@hotmail.com>
2021-06-24 18:52:17 +0200

    refactor: simplify tests

--- shared/go/pkg/server/api_spa_server_test.go
+++ shared/go/pkg/server/api_spa_server_test.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 	"time"
 
@@ -21,24 +20,6 @@ type APIRes struct {
 	shouldPass   bool
 }
 
-func TestAPI404(t *testing.T) {
-	s := NewAPISPAServer("8080")
-
-	r := &s.Router
-	r.HandleFunc("/", http.NotFound)
-
-	h := s.prepare(false).Handler
-	ts := httptest.NewServer(h)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, res.StatusCode, 404)
-}
-
 var apiTestcases = []APIRes{
 	{"/api/v1/status", "/api/v1/status", "OK", "OK", true},
 	{"/api/v1/status", "/api/v1/status", "OK", "Ok", false},
@@ -47,37 +28,73 @@ var apiTestcases = []APIRes{
 
 // TODO: do something reasonable here
 func TestAPI(t *testing.T) {
-	for _, test := range apiTestcases {
+	for i, test := range apiTestcases {
+		t.Run(fmt.Sprintf("Test API No %v", i), func(t *testing.T) {
+			s := NewAPISPAServer("8080")
+
+			r := &s.Router
+			r.HandleFunc(test.apiRoute, func(w http.ResponseWriter, r *http.Request) {
+				res := []byte(test.apiResponse)
+				w.Write(res)
+			}).Methods("GET")
+			r.HandleFunc("/", http.NotFound).Methods("GET")
+
+			h := s.prepare(false).Handler
+			ts := httptest.NewServer(h)
+			defer ts.Close()
+
+			res, err := http.Get(ts.URL + test.requestRoute)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			body, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if test.shouldPass {
+				assert.Equal(t, test.expected, string(body))
+			} else {
+				assert.NotEqual(t, test.expected, string(body))
+			}
+		})
+	}
+	t.Run("Test API return 404", func(t *testing.T) {
 		s := NewAPISPAServer("8080")
 
 		r := &s.Router
-		r.HandleFunc(test.apiRoute, func(w http.ResponseWriter, r *http.Request) {
-			res := []byte(test.apiResponse)
-			w.Write(res)
-		}).Methods("GET")
-		r.HandleFunc("/", http.NotFound).Methods("GET")
+		r.HandleFunc("/", http.NotFound)
 
 		h := s.prepare(false).Handler
 		ts := httptest.NewServer(h)
 		defer ts.Close()
 
-		res, err := http.Get(ts.URL + test.requestRoute)
+		res, err := http.Get(ts.URL + "/")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		body, err := ioutil.ReadAll(res.Body)
-		res.Body.Close()
+		assert.Equal(t, res.StatusCode, 404)
+	})
+	t.Run("Test API return 200", func(t *testing.T) {
+		s := NewAPISPAServer("8080")
+
+		r := &s.Router
+		r.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) { io.WriteString(rw, "OK") })
+
+		h := s.prepare(false).Handler
+		ts := httptest.NewServer(h)
+		defer ts.Close()
+
+		res, err := http.Get(ts.URL + "/status")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if test.shouldPass {
-			assert.Equal(t, test.expected, string(body))
-		} else {
-			assert.NotEqual(t, test.expected, string(body))
-		}
-	}
+		assert.Equal(t, res.StatusCode, 200)
+	})
 }
 
 func TestSPA(t *testing.T) {
@@ -97,28 +114,6 @@ func TestSPA(t *testing.T) {
 	}()
 	time.Sleep(100 * time.Millisecond)
 
-	t.Run("Testing 404 on API Route", func(t *testing.T) {
-		rs := make(chan int, 1)
-		go func() {
-			res, _ := http.Get("http://localhost:8080/api")
-			rs <- res.StatusCode
-		}()
-
-		code := <-rs
-		assert.Equal(t, 404, code)
-	})
-
-	t.Run("Testing 200 on API Route", func(t *testing.T) {
-		rs := make(chan int, 1)
-		go func() {
-			res, _ := http.Get("http://localhost:8080/api/v1")
-			rs <- res.StatusCode
-		}()
-
-		code := <-rs
-		assert.Equal(t, 200, code)
-	})
-	fmt.Println(os.Getwd())
 	t.Run("Testing SPA Route", func(t *testing.T) {
 		rs := make(chan int, 1)
 		go func() {
