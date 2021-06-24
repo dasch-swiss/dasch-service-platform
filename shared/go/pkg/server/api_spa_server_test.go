8b2ebc919dc46428adab69d4b1009592b825ed78
Balduin Landolt <balduin.landolt@hotmail.com>
2021-06-24 19:45:43 +0200

    feat: improve tests

--- shared/go/pkg/server/api_spa_server_test.go
+++ shared/go/pkg/server/api_spa_server_test.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -26,7 +25,6 @@ var apiTestcases = []APIRes{
 	{"/", "/", "OK", "Ok", false},
 }
 
-// TODO: do something reasonable here
 func TestAPI(t *testing.T) {
 	for i, test := range apiTestcases {
 		t.Run(fmt.Sprintf("Test API No %v", i), func(t *testing.T) {
@@ -97,32 +95,113 @@ func TestAPI(t *testing.T) {
 	})
 }
 
+type SPARes struct {
+	desc        string
+	request     string
+	expFile     string
+	contentType string
+}
+
+var spaTestcases = []SPARes{
+	{"Test root", "/", "index.html", "text/html; charset=utf-8"},
+	{"Test root-index", "/index.html", "index.html", "text/html; charset=utf-8"},
+	{"Test root-css", "/global.css", "global.css", "text/css; charset=utf-8"},
+	{"Test subfolder-css", "/build/bundle.css", "build/bundle.css", "text/css; charset=utf-8"},
+	{"Test subfolder-js", "/build/bundle.js", "build/bundle.js", "application/javascript"},
+	{"Test spa-route-index-1", "/project/index.html", "index.html", "text/html; charset=utf-8"},
+	{"Test spa-route-index-2", "/project/admin/index.html", "index.html", "text/html; charset=utf-8"},
+	{"Test spa-route-index-3", "/project/admin/id/1234/index.html", "index.html", "text/html; charset=utf-8"},
+	{"Test spa-route-css", "/project/admin/id/1234/global.css", "global.css", "text/css; charset=utf-8"},
+	{"Test spa-route-subfolder-css", "/project/admin/id/1234/build/bundle.css", "build/bundle.css", "text/css; charset=utf-8"},
+	{"Test query", "/projects?_page=1&_limit=9", "index.html", "text/html; charset=utf-8"},
+	{"Test non-existing-file", "/something/nonsense.txt", "index.html", "text/html; charset=utf-8"},
+}
+
 func TestSPA(t *testing.T) {
+
 	s := NewAPISPAServer("8080")
+	s.SetSPA("testfiles")
 
-	r := &s.Router
+	h := s.prepare(false).Handler
+	ts := httptest.NewServer(h)
+	defer ts.Close()
 
-	// s.SetSPA("../../../../public")
-	s.SetSPA("../../../../shared/go/pkg/server/testfiles")
+	for _, test := range spaTestcases {
+		t.Run(test.desc, func(t *testing.T) {
+			res, err := http.Get(ts.URL + test.request)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Run("Test status code", func(t *testing.T) {
+				assert.Equal(t, 200, res.StatusCode)
+			})
+			t.Run("Test content-type", func(t *testing.T) {
+				assert.Equal(t, test.contentType, res.Header.Get("Content-Type"))
+			})
+			t.Run("Test content", func(t *testing.T) {
+				body, _ := ioutil.ReadAll(res.Body)
+				exp, err := ioutil.ReadFile("testfiles/" + test.expFile)
+				if err != nil {
+					t.Fatal("Could not read reference file `index.html` from disc")
+				}
+				assert.Equal(t, exp, body)
+			})
+		})
+	}
 
-	r.HandleFunc("/api", http.NotFound)
-	r.HandleFunc("/api/v1", func(rw http.ResponseWriter, r *http.Request) { io.WriteString(rw, "OK") })
+	// t.Run("Test Root", func(t *testing.T) {
+	// 	res, err := http.Get(ts.URL + "/")
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// 	t.Run("Test status code", func(t *testing.T) {
+	// 		assert.Equal(t, 200, res.StatusCode)
+	// 	})
+	// 	t.Run("Test content", func(t *testing.T) {
+	// 		// assert.Equal(t, 200, res.StatusCode)
+	// 		body, _ := ioutil.ReadAll(res.Body)
+	// 		exp, err := ioutil.ReadFile("testfiles/index.html")
+	// 		if err != nil {
+	// 			t.Fatal("Could not read reference file `index.html` from disc")
+	// 		}
+	// 		assert.Equal(t, exp, body)
+	// 	})
+	// })
 
-	go func() {
-		fmt.Println("Serving...")
-		s.ListenAndServe()
-	}()
-	time.Sleep(100 * time.Millisecond)
+	// t.Run("Test Root-index", func(t *testing.T) {
+	// 	res, err := http.Get(ts.URL + "/index.html")
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// 	t.Run("Test status code", func(t *testing.T) {
+	// 		assert.Equal(t, 200, res.StatusCode)
+	// 	})
+	// 	t.Run("Test content", func(t *testing.T) {
+	// 		// assert.Equal(t, 200, res.StatusCode)
+	// 		body, _ := ioutil.ReadAll(res.Body)
+	// 		exp, err := ioutil.ReadFile("testfiles/index.html")
+	// 		if err != nil {
+	// 			t.Fatal("Could not read reference file `index.html` from disc")
+	// 		}
+	// 		assert.Equal(t, exp, body)
+	// 	})
+	// })
 
-	t.Run("Testing SPA Route", func(t *testing.T) {
-		rs := make(chan int, 1)
-		go func() {
-			res, _ := http.Get("http://localhost:8080/public/global.css")
-			rs <- res.StatusCode
-		}()
+	// go func() {
+	// 	fmt.Println("Serving...")
+	// 	s.ListenAndServe()
+	// }()
+	// time.Sleep(100 * time.Millisecond)
 
-		code := <-rs
-		assert.Equal(t, 200, code)
-	})
+	// t.Run("Testing SPA Route", func(t *testing.T) {
+	// 	rs := make(chan int, 1)
+	// 	go func() {
+	// 		res, _ := http.Get("http://localhost:8080/public/global.css")
+	// 		rs <- res.StatusCode
+	// 	}()
+
+	// 	code := <-rs
+	// 	assert.Equal(t, 200, code)
+	// })
 
 }
